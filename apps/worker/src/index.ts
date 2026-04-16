bd9fd2f2037912b5805c8d7e46b8dee0f1dc1fba
Vansh Sharma <sharmavansh.3777@gmail.com>
2026-04-16 14:16:57 +0530

    added fake web server

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,7 +1,14 @@
-console.log("starting all workers")
+import { serve } from "bun";
 
 import "./workers/storeToDb.worker"
 import "./workers/storeSongsToDb.worker"
 import "./workers/storeAlbumsToDb.worker"
 
 console.log("All workers started")
+
+serve({
+  port: Number(process.env.PORT) || 3001,
+  fetch(req) {
+    return new Response("Worker is running 🚀");
+  },
+});
